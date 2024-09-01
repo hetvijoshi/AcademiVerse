db0b87c11ceb49cee1c79fd57d1952dfb684cf18
Ekta Patel <architectekta@gmail.com>
2024-08-31 23:14:32 -0500

    Backend

---
+++ academiverse-api/src/main/java/com/academiverse/academiverse_api/AcademiverseApiApplication.java
@@ -0,0 +1,13 @@
+package com.academiverse.academiverse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademiverseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademiverseApiApplication.class, args);
+	}
+
+}
