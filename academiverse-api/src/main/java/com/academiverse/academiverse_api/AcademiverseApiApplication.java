70cb4132e4b435c3c082854f7fe0fee9b30a72e7
Hetvi Joshi <hetvijoshi510@gmail.com>
2024-09-02 16:42:03 -0500

    Changed auto entity configuration settings

--- academiverse-api/src/main/java/com/academiverse/academiverse_api/AcademiverseApiApplication.java
+++ academiverse-api/src/main/java/com/academiverse/academiverse_api/AcademiverseApiApplication.java
@@ -2,8 +2,9 @@ package com.academiverse.academiverse_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AcademiverseApiApplication {
 
 	public static void main(String[] args) {
